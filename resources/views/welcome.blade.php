dfd960faf13ebdeae876c483e57b3c00bdb01a95
WahibJan <167640356+WahibSa@users.noreply.github.com>
2025-10-26 18:12:51 +0700

    Update greeting text in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-       Hello World
+       Hello World Dari temen
 
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
